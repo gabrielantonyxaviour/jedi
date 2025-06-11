c6393f85a491b345a6892972fec6536e05964a14
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-06-11 03:24:11 +0200

    feat: update metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Jedi | Build your AI co-founder",
+  description: "Build your AI co-founder",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
